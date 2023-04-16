a9a925c6dbccdda10617beedf9a0ca8ca113b535
SarraBarnett <sarrabarnett@gmail.com>
2023-04-16 07:56:14 +0000

    configured remote backend to terraform cloud, and aws provider

---
+++ terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "aws-infrastructure"
+
+    workspaces {
+      name = "two-tier-terraform"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
