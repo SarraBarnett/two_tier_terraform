9d5f485d245f1b27c7c98d9e78019e11d23c9451
SarraBarnett <sarrabarnett@gmail.com>
2023-04-20 07:00:00 +0000

    configured ec2 instance child module with an aws instance resource block

---
+++ modules/ec2_instance/main.tf
@@ -0,0 +1,19 @@
+# EC2 Instance Child Module 
+
+resource "aws_instance" "web_server" {
+  count = length(var.subnet_ids) /* creates an ec2 instance with each instance 
+  assigned to a different subnet ID in the subnet_ids list */
+
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [var.web_server_sg_id]
+  subnet_id              = var.subnet_ids[count.index]
+
+  user_data = var.user_data
+
+  tags = {
+    Name = "${var.name_prefix}-${count.index}"
+  }
+}
