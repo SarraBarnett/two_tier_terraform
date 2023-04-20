7150e60ef612cd96d86b74bdb983fad2ec46a8b9
SarraBarnett <sarrabarnett@gmail.com>
2023-04-20 07:02:20 +0000

    defined rds instance output blocks

---
+++ modules/rds_instance/outputs.tf
@@ -0,0 +1,20 @@
+# RDS Instance Child Module - Output Variables
+
+output "rds_endpoint" {
+  value = aws_db_instance.rds.endpoint
+}
+
+output "rds_port" {
+  value = aws_db_instance.rds.port
+}
+
+output "rds_username" {
+  value = aws_db_instance.rds.username
+}
+
+output "rds_password" {
+  value = aws_db_instance.rds.password
+}
+
+/* an output variable called db_instance that will be referenced by
+the aws_db_instance resource in the child module. */
