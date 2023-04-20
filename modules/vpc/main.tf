691ded9c71adb8d84744f3abbfc932b0b6906022
SarraBarnett <sarrabarnett@gmail.com>
2023-04-20 07:07:49 +0000

    configured vpc child module with vpc, subnets, internet gateway, route table, route table associations, EIP, and NAT gateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,8 +1,86 @@
-resource "aws_vpc" "project_vpc" {
+# Child Module VPC 
+
+
+resource "aws_vpc" "vpc" {
   cidr_block = var.cidr_block
   tags = {
-    Name = "project_vpc"
+    Name = var.name
   }
 }
 
-variable "cidr_block" {}
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.public_subnet_cidr[0]
+  availability_zone = "${var.region}a"
+  tags = {
+    Name = "${var.name}-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.public_subnet_cidr[1]
+  availability_zone = "${var.region}b"
+  tags = {
+    Name = "${var.name}-public-subnet-2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet_cidr[0]
+  availability_zone = "${var.region}a"
+  tags = {
+    Name = "${var.name}-private-subnet-1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet_cidr[1]
+  availability_zone = "${var.region}b"
+  tags = {
+    Name = "${var.name}-private-subnet-2"
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.name}-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+  tags = {
+    Name = "${var.name}-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "${var.name}-nat-gateway"
+  }
+}
