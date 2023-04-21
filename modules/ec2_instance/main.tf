bd43dc57b8a03c9dc139a1e210237ff9b8fd8a92
SarraBarnett <sarrabarnett@gmail.com>
2023-04-21 23:02:44 +0000

    added argument to create public ip address to each instance

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "web_server" {
 
   ami           = var.ami
   instance_type = var.instance_type
+  associate_public_ip_address = true
   key_name      = var.key_name
 
   vpc_security_group_ids = [var.web_server_sg_id]
