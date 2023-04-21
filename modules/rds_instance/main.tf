3bfa888221d85fafc0f1790c86f2c70d4853f27b
SarraBarnett <sarrabarnett@gmail.com>
2023-04-21 05:40:46 +0000

    syntax change

--- modules/rds_instance/main.tf
+++ modules/rds_instance/main.tf
@@ -6,8 +6,8 @@ resource "aws_db_instance" "rds" {
   engine_version         = var.engine_version
   instance_class         = var.instance_class
   db_name                = var.db_name
-  db_username               = var.db_username
-  db_password               = var.db_password
+  username               = var.db_username
+  password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
   vpc_security_group_ids = [var.security_group_id]
 }
