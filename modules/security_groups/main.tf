c443b19b8fa914ed9a9478dc53c1471b7bc090ee
SarraBarnett <sarrabarnett@gmail.com>
2023-04-22 06:10:20 +0000

    changed port

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "web_server_sg" {
 resource "aws_security_group_rule" "web_server_ingress" {
   type              = "ingress"
   from_port         = var.web_server_ingress_ports[0]
-  to_port           = var.web_server_ingress_ports[1]
+  to_port           = var.web_server_ingress_ports[0]
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.web_server_sg.id
