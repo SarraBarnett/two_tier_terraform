7c0221cad4215b5364de3c8ccdb4128705ff12d6
SarraBarnett <sarrabarnett@gmail.com>
2023-04-22 02:29:03 +0000

    added ssh ingress rule

--- modules/security_groups/main.tf
+++ modules/security_groups/main.tf
@@ -35,6 +35,15 @@ resource "aws_security_group_rule" "web_server_egress" {
   source_security_group_id = aws_security_group.rds_sg.id
 }
 
+resource "aws_security_group_rule" "ssh_ingress" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web_server_sg.id
+}
+
 # Define security group for RDS database:
 
 resource "aws_security_group" "rds_sg" {
