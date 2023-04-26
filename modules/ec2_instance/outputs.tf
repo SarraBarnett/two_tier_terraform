8cf2aab95c87d6c6c8137b492e76e5887c9935d9
SarraBarnett <sarrabarnett@gmail.com>
2023-04-26 01:07:57 +0000

    deleted uneeded outputs

--- modules/ec2_instance/outputs.tf
+++ modules/ec2_instance/outputs.tf
@@ -1,16 +1,8 @@
 # EC2 Instance Child Module - Output Variables
 
-output "instance_id" {
-  value = aws_instance.web_server[*].id
-}
-
-output "public_ips" {
-  description = "Public IP addresses of the EC2 instances"
-  value       = aws_instance.web_server[*].public_ip
-}
-
-output "private_ip" {
-  value = aws_instance.web_server[*].private_ip
+output "public_ip" {
+  description = "Public IP address of EC2 instance"
+  value       = aws_instance.web.*.public_ip
 }
 
 /* use the [*] syntax to include all instances of the aws_instance.web_server 
