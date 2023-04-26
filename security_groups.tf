707f12718d20bd8bfd0202749f57f95f8d591875
SarraBarnett <sarrabarnett@gmail.com>
2023-04-26 01:07:41 +0000

    added security groups file

---
+++ security_groups.tf
@@ -0,0 +1,68 @@
+# Define security group rules for EC2 instance
+resource "aws_security_group" "ec2_sg" {
+  name_prefix = "ec2_sg_"
+
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "ec2_ingress" {
+  security_group_id = aws_security_group.ec2_sg.id
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+# Define security group rules for RDS instance
+resource "aws_security_group" "rds_sg" {
+  name_prefix = "rds_sg_"
+
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "rds_ingress" {
+  security_group_id = aws_security_group.rds_sg.id
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "rds_egress" {
+  security_group_id = aws_security_group.rds_sg.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
