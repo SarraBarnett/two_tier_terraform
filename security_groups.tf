095142809a44c43603e1feb8cf43dd8ef0cc11d2
SarraBarnett <sarrabarnett@gmail.com>
2023-04-26 01:52:07 +0000

    deleted duplicate security group rule

--- security_groups.tf
+++ security_groups.tf
@@ -48,21 +48,3 @@ resource "aws_security_group" "rds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_security_group_rule" "rds_ingress" {
-  security_group_id = aws_security_group.rds_sg.id
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "rds_egress" {
-  security_group_id = aws_security_group.rds_sg.id
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
