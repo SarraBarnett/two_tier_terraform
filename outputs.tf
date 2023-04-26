a6afd07916ce7c68e6d7d6bc28596fc96183d467
SarraBarnett <sarrabarnett@gmail.com>
2023-04-26 01:07:08 +0000

    deleted uneeded outputs

--- outputs.tf
+++ outputs.tf
@@ -2,20 +2,10 @@
 
 output "web_server_public_ips" {
   description = "Public IP addresses of the web server instances"
-  value       = module.web_server.public_ips
+  value       = module.web_server.public_ip
 }
 
 output "rds_endpoint" {
   description = "The endpoint for the RDS MySQL instance"
   value       = module.rds_instance.rds_endpoint
-}
-
-output "security_group_id" {
-  description = "The ID of the security group for the RDS instance"
-  value       = module.security_groups.rds_sg_id
-}
-
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = module.vpc.vpc_id
 }
