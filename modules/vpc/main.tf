d95d37f0d28dd767b8a039398de5c892841ec65a
SarraBarnett <sarrabarnett@gmail.com>
2023-04-30 05:09:05 +0000

    added nat gateway to private route table

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -26,6 +26,12 @@ resource "aws_route_table" "public_route_table" {
 
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
   tags = {
     Name = "${var.name}-private-route-table"
   }
